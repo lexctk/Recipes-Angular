03eb1f5823cc197256a7c626d25b246bf6af33ea
lexctk <alexandra.derevko@gmail.com>
2018-12-18 18:42:05 +0100

    To Shopping List
    * Add ingredients to shopping list from recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -25,10 +26,14 @@ export class RecipeService {
         new Ingredient('Eggs', 6)])
   ];
 
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   getRecipes() {
     // get a copy
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList (ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
