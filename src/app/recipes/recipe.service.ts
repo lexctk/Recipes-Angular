8880e0be16b2a78c29795f21f60609dc95983f23
lexctk <alexandra.derevko@gmail.com>
2018-12-18 18:29:15 +0100

    HTML/CSS changes
    * Improve UX
    * Fix some css and spacing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +9,20 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This is just a test',
-      'https://s3.amazonaws.com/pixtruder/original_images/17c6ec7292ce74c4f38c71ee4816925c46ffd4bf')
+    new Recipe(
+      'Homemade Pizza',
+      'Super awesome pizza!',
+      'https://www.simplyrecipes.com/wp-content/uploads/2007/01/homemade-pizza-horiz-a-1200.jpg',
+      [
+        new Ingredient('Pepperoni', 13),
+        new Ingredient('Mushrooms', 15)]),
+    new Recipe(
+      'Apple Pie',
+      'Traditional Apple Pie!',
+      'https://img1.southernliving.timeinc.net/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/2017/10/main/arkansas_black_apple_pie_with_caramel_sauce_2540501_pieso_675.jpg?itok=e6AfBEXc',
+      [
+        new Ingredient('Apples', 5),
+        new Ingredient('Eggs', 6)])
   ];
 
   constructor() { }
